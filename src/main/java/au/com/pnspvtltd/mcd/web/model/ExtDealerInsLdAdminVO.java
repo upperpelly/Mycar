541d1574115ab1badf924d3d5f5417c050e1da94
pavan kumar <pavan.upperpelly@gmail.com>
2017-07-22 18:21:34 +0530

    sticky

---
+++ src/main/java/au/com/pnspvtltd/mcd/web/model/ExtDealerInsLdAdminVO.java
@@ -0,0 +1,21 @@
+package au.com.pnspvtltd.mcd.web.model;
+
+import java.sql.Date;
+import java.util.List;
+
+public class ExtDealerInsLdAdminVO {
+
+	List<ExtDealerSearchInsVO> extDealerSearchVO;
+
+	public List<ExtDealerSearchInsVO> getExtDealerSearchVO() {
+		return extDealerSearchVO;
+	}
+
+	public void setExtDealerSearchVO(List<ExtDealerSearchInsVO> extDealerSearchVO) {
+		this.extDealerSearchVO = extDealerSearchVO;
+	}
+	
+
+	
+	
+}
